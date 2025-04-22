7a6fa2695df9335e3f5437b2280a3bcf970138f4
cernoh <zoiny@outlook.com>
2025-04-22 13:18:36 +0100

    constant prompt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::io::Write;
 use std::io::stdin;
+use std::io::stdout;
 use std::process::Command;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("-> ");
+        stdout().flush();
 
-    let command = input.trim();
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    Command::new(command).spawn().unwrap();
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        child.wait();
+    }
 }
